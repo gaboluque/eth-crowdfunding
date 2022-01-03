71b66ddae9f46543bd502e3e28e4846661222a77
gabolque <gluquemiranda@gmail.com>
2022-01-03 12:38:44 -0500

    Added create request function,using memory variables

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -8,11 +8,17 @@ contract Campaign {
         bool complete;
     }
 
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
 
-    function Campaign(uint minimum) public {
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
     }
@@ -20,6 +26,17 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value >= minimumContribution);
         approvers.push(msg.sender);
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+
+        requests.push(newRequest);
     }
 
 
