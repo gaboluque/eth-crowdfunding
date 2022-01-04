317e090c5084541cec289bcf720d4fca383b6a27
gabolque <gluquemiranda@gmail.com>
2022-01-04 14:42:12 -0500

    Added approveRequest function

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.8.11;
 
 contract Campaign {
     struct Request {
@@ -6,37 +6,48 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    mapping(address => bool) public approvers;
 
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    constructor(uint minimum) public {
+    constructor(uint minimum) {
         manager = msg.sender;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
         require(msg.value >= minimumContribution);
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
-        Request memory newRequest = Request({
-            description: description,
-            value: value,
-            recipient: recipient,
-            complete: false
-        });
+    function createRequest(string memory description, uint value, address recipient) public restricted {
+        Request storage newRequest = requests.push();
 
-        requests.push(newRequest);
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
+
+    function approveRequest(uint requestIndex) public {
+        Request storage request = requests[requestIndex];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount ++;
     }
 
 
