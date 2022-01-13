c09ba110682ffdeb34805589e77f26249c4ac4cb
gabolque <gluquemiranda@gmail.com>
2022-01-13 13:05:32 -0500

    Finalize and approve request

--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -1,7 +1,8 @@
 import {Button, Table} from "semantic-ui-react";
 import {useRouter} from "next/router";
-import Campaign from "../../../../ethereum/campaign";
 import web3 from "../../../../ethereum/web3";
+import Campaign from "../../../../ethereum/campaign";
+import {add} from "lodash/math";
 
 export async function getServerSideProps(context) {
   const {address} = context.query;
@@ -38,6 +39,24 @@ const CampaignRequests = ({address, requests, requestsCount, approversCount}) =>
     router.push(`/campaigns/${address}/requests/new`);
   }
 
+  const onApprove = (requestIndex) => async () => {
+    const campaign = Campaign(address);
+
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.approveRequest(requestIndex).send({
+      from: accounts[0]
+    })
+  }
+
+  const onFinalize = (requestIndex) => async () => {
+    const campaign = Campaign(address);
+
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.finalizeRequest(requestIndex).send({
+      from: accounts[0]
+    })
+  }
+
   return (
     <div>
       <Button primary onClick={onAddRequest}>Add request</Button>
@@ -54,19 +73,26 @@ const CampaignRequests = ({address, requests, requestsCount, approversCount}) =>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {requests.map((req, index) => (
-            <Table.Row key={req.description}>
-              <Table.Cell>{index}</Table.Cell>
-              <Table.Cell>{req.description}</Table.Cell>
-              <Table.Cell>{req.value} ETH</Table.Cell>
-              <Table.Cell>{req.recipient}</Table.Cell>
-              <Table.Cell>{req.approvalCount}/{approversCount}</Table.Cell>
-              <Table.Cell><Button primary>Approve</Button></Table.Cell>
-              <Table.Cell><Button>Finalize</Button></Table.Cell>
-            </Table.Row>
-          ))}
+          {requests.map((req, index) => {
+            const readyToFinalize = req.approvalCount > approversCount / 2;
+
+            return (
+              <Table.Row key={req.description} disabled={req.complete} positive={readyToFinalize && !req.complete}>
+                <Table.Cell>{index}</Table.Cell>
+                <Table.Cell>{req.description}</Table.Cell>
+                <Table.Cell>{req.value} ETH</Table.Cell>
+                <Table.Cell>{req.recipient}</Table.Cell>
+                <Table.Cell>{req.approvalCount}/{approversCount}</Table.Cell>
+                <Table.Cell>{req.complete ? null :
+                  <Button color="green" onClick={onApprove(index)}>Approve</Button>}</Table.Cell>
+                <Table.Cell>{req.complete ? "Finalized" :
+                  <Button color="orange" onClick={onFinalize(index)}>Finalize</Button>}</Table.Cell>
+              </Table.Row>
+            )
+          })}
         </Table.Body>
       </Table>
+      <div>{`Found ${requestsCount} requests`}</div>
     </div>
   );
 };
